42044214f7edf1d9fdab4dde71468fe8e743ddee
amit reuveni <amitr123r@gmail.com>
2026-03-06 13:14:37 +0200

    chore: initial project setup

---
+++ my-app/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <main className='flex min-h-screen items-center justify-center'>
+        <h1 className='text-4x1 font-bold'>Hello World</h1>
+      </main>
+    </div>
+  )
+}
+
+export default page
